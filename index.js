805b5957513adac927c19bf8d30405d5815c2f3a
Chester How <chesterhow@gmail.com>
2020-07-05 12:57:20 +0800

    implement pdf sending

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 require('dotenv').config();
 const { Telegram } = require('telegraf');
-const schedule = require('node-schedule');
+const puppeteer = require('puppeteer');
 
 const URL = 'https://lovesingapore.org.sg/40day/2020/';
-const CHANNEL_NAME = '@fortyday2020';
+// const CHANNEL_NAME = '@fortyday2020';
+const CHANNEL_NAME = '@chestestchannel';
 
-const bot = new Telegram(process.env.BOT_TOKEN);
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(URL);
+  await page._client.send('Page.setDownloadBehavior', {
+    behavior: 'allow',
+    downloadPath: './',
+  });
 
-const getMessage = () => {
-  const startDate = new Date(2020, 6, 1); // 1 JULY 2020
-  const currDate = new Date();
-  const daysDiff = Math.ceil(
-    (currDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24)
+  const pdfUrl = await page.$eval('.et_pb_text_inner > a', (a) =>
+    a.getAttribute('href')
   );
-  const month = currDate.toLocaleString('default', { month: 'long' });
-  const day = currDate.getDate();
 
-  return `📆 <b>Today's Prayer Guide</b> - <i>${month} ${day}, 2020 (Day ${daysDiff})</i>\n${URL}${month.toLowerCase()}-${day}`;
-};
+  const getMessage = () => {
+    const startDate = new Date(2020, 6, 1); // 1 JULY 2020
+    const currDate = new Date();
+    const daysDiff = Math.ceil(
+      (currDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24)
+    );
+    const month = currDate.toLocaleString('default', { month: 'long' });
+    const day = currDate.getDate();
 
-schedule.scheduleJob('0 7 * * *', function () {
+    return `📆 <b>Today's Prayer Guide</b> - <i>${month} ${day}, 2020 (Day ${daysDiff})</i>\n${URL}${month.toLowerCase()}-${day}`;
+  };
+
+  const bot = new Telegram(process.env.TEST_BOT_TOKEN);
+
   bot.sendMessage(CHANNEL_NAME, getMessage(), { parse_mode: 'HTML' });
-});
+  await bot.sendDocument(CHANNEL_NAME, pdfUrl);
+})();
