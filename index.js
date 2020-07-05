4c7f66fb10dbd856b7717679df7eebbbdb4d8091
Chester How <chesterhow@gmail.com>
2020-07-05 15:11:42 +0800

    production setup

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const { Telegram } = require('telegraf');
 const puppeteer = require('puppeteer');
 
 const URL = 'https://lovesingapore.org.sg/40day/2020/';
-// const CHANNEL_NAME = '@fortyday2020';
-const CHANNEL_NAME = '@chestestchannel';
+const CHANNEL_NAME = '@fortyday2020';
 
 async function run() {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
@@ -23,12 +22,11 @@ async function run() {
     );
     const month = currDate.toLocaleString('default', { month: 'long' });
     const day = currDate.getDate();
-    console.log(currDate.getHours());
 
     return `📆 <b>Today's Prayer Guide</b> - <i>${month} ${day}, 2020 (Day ${daysDiff})</i>\n${URL}${month.toLowerCase()}-${day}`;
   };
 
-  const bot = new Telegram(process.env.TEST_BOT_TOKEN);
+  const bot = new Telegram(process.env.BOT_TOKEN);
 
   bot
     .sendMessage(CHANNEL_NAME, getMessage(), { parse_mode: 'HTML' })
